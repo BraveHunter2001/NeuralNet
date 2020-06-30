8428be8ace761fec3c5c1509dc9ec07a7c9bd61e
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2020-06-30 17:59:14 +0300

    Добавьте файлы проекта.

---
+++ NeuralNet/Layer.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace NeuralNet
+{
+    public class Layer
+    {
+        public List<Neuron> Neurons { get; }
+        public int NeuronCount => Neurons?.Count ?? 0;
+
+        public NeuronType Type;
+
+        public Layer(List<Neuron> neurons, NeuronType type = NeuronType.Normal)
+        {
+            Neurons = neurons;
+            Type = type;
+        }
+
+        public List<double> GetSignals()
+        {
+            var result = new List<double>();
+            foreach (var neuron in Neurons)
+                result.Add(neuron.Output);
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return Type.ToString();
+        }
+    }
+}
---
+++ NeuralNet/NeuronType.cs
@@ -0,0 +1,11 @@
+﻿
+namespace NeuralNet
+{
+    public enum NeuronType
+        {
+            Input = 0,
+            Normal = 1,
+            Output = 2
+        }
+
+}
